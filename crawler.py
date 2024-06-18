66ec38c0ab7b58a5f8bd04c6f829ac9a1caec9d5
“minhduc2672002” <minhduc2672002@gmail.com>
2024-06-19 02:18:52 +0700

    update perform download

--- crawler.py
+++ crawler.py
@@ -9,25 +9,32 @@ from bs4 import BeautifulSoup
 import sys
 
 class Crawler:
-    def __init__(self, urls_path, urls_checklist,image_path, log_file='crawler.log',max_woker = 3):
+    def __init__(self, urls_path, urls_checklist,image_path, log_file='crawler.log',max_woker = 5):
         self.urls_path = urls_path
         self.urls_checklist = urls_checklist
         logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
         self.checklist_df = self.create_checklist()
         self.max_woker = max_woker
         self.image_path = image_path
-        self.total_product = 0
+        self.image_visited = set()
 
-    def polite_request(self, url):
+    def polite_request(self, url,time_out = 60):
+        time_count = 0
         while True:
+            if time_count >= time_out:
+                logging.infor(f"Request URLs {url} Failed!!!")
+                return None
             try:
                 response = requests.get(url)
                 response.raise_for_status()
                 return response
             except requests.exceptions.ConnectionError:
                 print("Network Disconnect!!!")
+                time_count += 5
+                time.sleep(5)
             except requests.exceptions.RequestException:
                 print(f"Request URLs {url} Error!!!")
+                time_count += 5
                 time.sleep(5)
 
     def get_totalpage_totalproducts(self, url):
@@ -55,10 +62,9 @@ class Crawler:
         df = pd.DataFrame(data)
         df.to_csv(file_path, mode=mode, index=False, header=header)
 
-    def crawl_all_products(self, url, file_path):
+    def crawl_all_products(self, url, file_path,index):
         logging.info("---------------------------------------------------")
         logging.info(f"START SCRAP URL: {url} ")
-
         total_page, total_product = self.get_totalpage_totalproducts(url)
         if total_page and total_product:
             urls = [url + f'?p={i}' if i > 1 else url for i in range(1, total_page + 1)]
@@ -66,12 +72,14 @@ class Crawler:
                 futures = [executor.submit(self.get_infor, url) for url in urls]
                 for future in concurrent.futures.as_completed(futures):
                     data = future.result()
+                    images_dowload = [item for item in data if item not in self.image_visited]
+                    self.image_visited.update(data)
                     self.save_to_csv(data, file_path)
-                    self.crawl_image(file_path,data)
+                    self.crawl_image(file_path,images_dowload)
             logging.info(f"Scraping Complete  {total_product} product of url: {url} ")
-            return total_product
+            return index,file_path
         
-        return 0
+        return index,file_path
 
     def process_data(self, file_path):
         if os.path.exists(file_path):
@@ -105,35 +113,24 @@ class Crawler:
         if response.status_code == 200:
             with open(save_path, 'wb') as file:
                 file.write(response.content)
-            logging.info(f"Photos have been downloaded and stored at: {save_path}")
-        else:
-            logging.info(f"Error: Unable to load image from {image_url}")
+
 
     def download_image_if_not_exists(self,product_name,link, product_image_folder):
         name = f"{product_name.strip().replace(' ', '-')}{link.split('/')[-1]}"
         save_path = os.path.join(product_image_folder, name)
-        if not os.path.exists(save_path):
-            self.download_image(link, save_path)
-            return f"Dowloaded {name}"
+        self.download_image(link, save_path)
+        return f"Dowloaded {name}"
 
     def crawl_image(self, file_path,products_infor = None):
-        df = pd.read_csv(file_path, header=None)
-        df.drop_duplicates(inplace=True)
-        df.columns = ['product_name', 'image_link']
-
-        product_image_folder = os.path.join(self.image_path, file_path.split(os.sep)[-1].split('.')[0])
+        
 
         if not os.path.exists(self.image_path):
             os.makedirs(self.image_path)
-        if not os.path.exists(product_image_folder):
-            os.makedirs(product_image_folder)
 
         logging.info("---------------------------------------------------")
         logging.info(f"START DOWNLOAD IMAGE IN FILE: {file_path} ")
-        
-
         with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-            futures = [executor.submit(self.download_image_if_not_exists,product_name,link, product_image_folder) for product_name,link in products_infor]
+            futures = [executor.submit(self.download_image_if_not_exists,product_name,link,self.image_path) for product_name,link in products_infor]
             for future in concurrent.futures.as_completed(futures):
                 data = future.result()
                 print(data)
@@ -142,7 +139,7 @@ class Crawler:
     def run(self):
         logging.info("*****************************************")
         logging.info("Crawl Session Begining")
-        with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=self.max_woker) as executor:
             futures = []
             for index, row in self.checklist_df.iterrows():
                 url = row['url']
@@ -152,12 +149,11 @@ class Crawler:
                 if status == 'DOING':
                     name_file = url.split('/')[-2]
                     file_path = os.path.join(os.getcwd(), 'data', f'{name_file}.csv')
-                    futures.append(executor.submit(self.crawl_all_products, url, file_path))
-                    self.process_data(file_path)
-                    status = self.update_status(index, 'DONE')
+                    futures.append(executor.submit(self.crawl_all_products, url, file_path,index))
             for future in concurrent.futures.as_completed(futures):
-                result = future.result()
-                self.total_product += result if isinstance(result, int) else 0
+                index,file_path = future.result()
+                self.update_status(index, 'DONE')
+                self.process_data(file_path)
 
         logging.info("*****************************************")
         logging.info("Crawl Session Finish")
@@ -168,8 +164,8 @@ if __name__ == "__main__":
     urls_checklist = os.path.join(os.getcwd(), 'urls_checklist.csv')
     image_path = os.path.join(os.getcwd(), 'data', 'image')
     crawler = Crawler(urls_path, urls_checklist,image_path)
-
     start_time = time.time()
     crawler.run()
     end_time = time.time()
-    print(f'Đã crawl {crawler.total_product} product trong {end_time - start_time} s')
+    logging.info(f'Đã crawl {len(crawler.image_visited)} product trong {end_time - start_time} s')
+    print(f'Finish crawl {len(crawler.image_visited)} product trong {end_time - start_time} s')
